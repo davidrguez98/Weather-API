1f83d58fa9be990c3898018051f0e1afaec571a7
davidrguez98 <ropeda98@gmail.com>
2025-02-25 16:38:43 +0100

    backend completo sin errores

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import requests
 from datetime import datetime
 
-def Weather_report(location):
-    
+def get_weather_data(location, days_range):
+
     base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
     key = "AZ94WHHAAH8V95MBDN63SU5DK"
 
     today = datetime.now().strftime("%Y-%m-%d")
-    range_days = input("Rango de días de previsión: ")
-    print(type(range_days))
     
-    location_url = f"{base_url}/{location}/{today}/next{range_days}days?unitGroup=metric&key={key}" #Tengo que añadir el día de mañana e intentar conseguir el rango
+    location_url = f"{base_url}/{location}/{today}/next{days_range}days?unitGroup=metric&key={key}" #Tengo que añadir el día de mañana e intentar conseguir el rango
     
     weather_data = requests.get(location_url).json()
+
+    return weather_data
+
+def format_data(weather_data):
 
     report = {
         "location": weather_data.get("address"),
@@ -42,7 +44,9 @@ def Weather_report(location):
 
         report["information"].append(day_info)
 
+    return report
 
+def display_report(report):
 
     print(f"\nMeteorología en {report["location"]}:")
     for day in report["information"]:
@@ -52,6 +56,14 @@ def Weather_report(location):
         print(f"Min temp.: {day["Min temp."]}°C")
         print(f"Precipitation probability: {day["Precipitation probability"]}°C")
 
+def main():
 
-location = input("Escribe una ciudad: ").capitalize()
-Weather_report(location)
+    location = input("Escribe una ciudad: ").capitalize()
+    days_range = int(input("Rango de días de previsión: ")) - 1
+    days_range_str = str(days_range)
+
+    weather_data = get_weather_data(location, days_range_str)
+    report = format_data(weather_data)
+    display_report(report)
+
+main()
